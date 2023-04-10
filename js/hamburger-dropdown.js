1cea3f5bc31ae0e84a812f660da85666d71462b1
daltonjatc22 <114698602+daltonjatc22@users.noreply.github.com>
2023-04-10 12:34:35 -0600

    Update

--- js/hamburger-dropdown.js
+++ js/hamburger-dropdown.js
@@ -1,1 +1,16 @@
-/* Commit */
+let dropDownButton = document.getElementById("drop-down-button");
+let dropDownContents = document.getElementById("hamburger-dropdown-contents-wrapper");
+
+let dropDownIsOpen = false;
+dropDownContents.style.right = "-100%"; // backup
+
+dropDownButton.onclick = function(){
+    if(dropDownIsOpen){
+        dropDownContents.style.right = "-100%"; // backup
+    }else {
+        dropDownContents.style.right = "0%";
+    }
+
+    dropDownIsOpen = !dropDownIsOpen;
+
+}
